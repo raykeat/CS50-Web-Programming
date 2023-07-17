e3ec5f6974eb7316872fd2ce57991dfbc43b589e
raykeat <teohraykeat@gmail.com>
2023-07-17 19:14:36 +0800

    CS50 Final Project (Netflix Clone) - Completed!

--- netflixpage/models.py
+++ netflixpage/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class Genres(models.Model):
-    genreid = models.IntegerField(max_length=100,default=0)
+class GenresMovies(models.Model):
+    genreid = models.IntegerField(default=0)
     genre = models.CharField(max_length=100)
+    movie_data_retrieved = models.BooleanField(default=False)
 
 
 class Movies(models.Model):
-    movieid = models.IntegerField(max_length=100,default=0)
+    movieid = models.IntegerField(default=0)
     title = models.CharField(max_length=300)
     genres = models.CharField(max_length=300)
     overview = models.TextField(max_length=300)
@@ -21,6 +22,21 @@ class Movies(models.Model):
     cast = models.TextField(max_length=300,null=True)
     duration = models.IntegerField(default=100,null=True)
     descriptiveadjectives = models.TextField(max_length=300,null=True)
+
+class GenresTVShows(models.Model):
+    genreid = models.IntegerField(default=0)
+    genre = models.CharField(max_length=100)
+    tvshow_data_retrieved = models.BooleanField(default=False)
+
+class TVShows(models.Model):
+    tvshowid = models.IntegerField(default=0)
+    title = models.CharField(max_length=300)
+    genres = models.CharField(max_length=300)
+    overview = models.TextField(max_length=300)
+    releasedate = models.CharField(max_length=300)
+    rating = models.FloatField(default=0)
+    posterpath = models.TextField(max_length=300)
+    embedurl = models.CharField(max_length=300,blank=True)
 
 class User(AbstractUser):
     groups = models.ManyToManyField(
@@ -42,8 +58,12 @@ class User(AbstractUser):
         help_text=('Specific permissions for this user.'),
         related_query_name='custom_user'
     )
-    liked_movies = models.ManyToManyField(Movies,related_name="likeduser")
-    watchlist_movies = models.ManyToManyField(Movies,related_name="watchlistuser")
+    liked_movies = models.ManyToManyField(Movies,related_name="likedusermovie")
+    watchlist_movies = models.ManyToManyField(Movies,related_name="watchlistusermovie")
+
+    liked_tvshows = models.ManyToManyField(TVShows,related_name="likedusertvshow")
+    watchlist_tvshows = models.ManyToManyField(TVShows,related_name="watchlistusertvshow")
+
 
 
 
